fccc3896c570bceacc4d01387b88d0ac833ad4f1
petarmarinov <petar.marinov@bet365.com>
2020-10-01 15:17:16 +0300

    gorilla mux

--- staticwebservice/go.mod
+++ staticwebservice/go.mod
@@ -1,3 +1,5 @@
 module mygolangtour/staticwebservice
 
 go 1.13
+
+require github.com/gorilla/mux v1.8.0
--- staticwebservice/main.go
+++ staticwebservice/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
@@ -11,7 +12,7 @@ const domain = "localhost"
 const port = "3000"
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./staticwebservice/root/")))
+	http.Handle("/files", http.FileServer(http.Dir("./staticwebservice/root/")))
 
 	http.HandleFunc("/json", func(writer http.ResponseWriter, r *http.Request) {
 		names := r.URL.Query()["name"]
@@ -36,6 +37,28 @@ func main() {
 	http.HandleFunc("/user", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("User page"))
 	})
+
+	//gorilla mux
+	r := mux.NewRouter()
+
+	productsSubrouter := r.PathPrefix("/products").Methods("GET").Subrouter()
+
+	productsSubrouter.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("V1 products page"))
+	}).
+		MatcherFunc(func(request *http.Request, match *mux.RouteMatch) bool {
+			apiVersion := request.Header.Get("api-version")
+			return apiVersion == "1.0" || apiVersion == "2.0"
+		}).
+		Schemes("http")
+
+	productsSubrouter.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("V3 products page"))
+	}).
+		Headers("api-version", "3.0").
+		Schemes("http")
+
+	http.Handle("/", r)
 
 	fmt.Println("Starting Web Service on: ", "http://"+domain+":"+port)
 	err := http.ListenAndServe(domain+":"+port, nil)
